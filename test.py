5c3c466907af8d11ea50bee6030d1581523664fe
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-06-24 16:20:50 -0500

    test

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import requests
 
-BASE_URL = "http://localhost:8081"
+BASE_URL = "http://52.205.152.205:8080"
 
 # Login
 login_data = {
-    "User_mail": "ascorread1",
+    "User_mail": "allan2",
     "password": "1234"
 }
 
-login_response = requests.post("http://localhost:8080/login", json=login_data)
+login_response = requests.post("http://52.203.72.116:8080/login", json=login_data)
 if login_response.status_code != 200:
     print("Error:", login_response.status_code, login_response.json())
     exit()
